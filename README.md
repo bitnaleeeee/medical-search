f5074b71ddc08dc7b14727d1fc29f0c08fb15957
bitnaleeeee <>
2022-11-24 19:42:06 +0900

    검색창구현

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## 질환명 검색시 API 호출 통해서 검색어 추천 기능 구현
 
 1. 캐싱, react-query, HTTP Client(axios)
-2. styled-components 사용
+2. Sass 사용
 3. 입력마다 API 호출하지 않도록 API 호출 횟수를 줄이는 전략 수립 및 실행
 4. API를 호출할 때 마다 console.info("calling api") 출력을 통해 콘솔창에서 API 호출 횟수 확인이 가능하도록 설정
--- src/Page/Search.js
+++ src/Page/Search.js
@@ -1,7 +1,23 @@
-import react from "react";
-import styled from "styled-components";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+import './Search.scss';
 
 const Search = () => {
-  return <>Hi there</>;
+  return (
+    <div className="searchWrap">
+      <div className="searchBox">
+        <div className="title">
+          국내 모든 임상시험 검색하고 <br />
+          온라인으로 참여하기
+        </div>
+        <div className="inputWrap">
+          <input className="inputBox" placeholder="질환명을 입력해주세요" />
+          <FontAwesomeIcon className="iconStyle" icon={faMagnifyingGlass} />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Search;
